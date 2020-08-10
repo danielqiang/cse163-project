19921b40fa7ef324d2c97ee70e83e47afc3244eb
Guitarjun <arjunsrivastava01@gmail.com>
2020-08-09 22:37:21 -0700

    Question 2 done

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ def download_csv(url: str) -> pd.DataFrame:
     return df
 
 
-# This method will combine any data set with the US states geospatial data set based on a specific column for each
-# (This method can be used for making visualizations for the project)
 def combine_state_data(state_col: str, data_col: str, data, mainland=True) -> gpd.GeoDataFrame:
     """
     Merges any data set with the US States geospatial data
@@ -50,42 +48,50 @@ def update_geo(df: gpd.GeoDataFrame, to_drop: pd.Series, latest=False):
 
 
 def main():
+    # Data URLs
     us_states_data_url = 'https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv'
     influenza_data_url = 'https://data.cdc.gov/api/views/ks3g-spdg/rows.csv?accessType=DOWNLOAD'
+    world_data_url = 'https://covid.ourworldindata.org/data/owid-covid-data.csv'
 
+    # world_data = download_csv(world_data_url)
     # us_states_data = download_csv(us_states_data_url)
+
+    # Local versions
     us_states_df = pd.read_csv('us_states_data.csv')
+    world_data = pd.read_csv(world_data_url)
 
-    # print(us_states_data.to_string())
+    # QUESTION 2
 
-    # Generate graphs for question 2 (Minnesota)
+    # DataFrame for Minnesota cases; convert to Time Series
     minnesota_mask = us_states_df['state'] == 'Minnesota'
     minnesota = us_states_df[minnesota_mask]
     minnesota.index = pd.to_datetime(minnesota['date'])
-    # print(minnesota)
-    # minnesota['cases'].plot()
-    # plt.title('Minnesota Cases')
-    # plt.savefig('minnesota_cases.png')
-
-    # # Training set
 
     # Replace dates with numeric counter for use in regression model training
-    minnesota = minnesota.assign(date=range(len(minnesota)))
+    minnesota_numeric = minnesota.assign(date=range(len(minnesota)))
 
-    pre_floyd = minnesota.loc[:'2020-05-25']
-    post_floyd = minnesota.loc['2020-05-26':]
-    pre_floyd_x, pre_floyd_y = pre_floyd['date'], pre_floyd['cases']
-    post_floyd_x, post_floyd_y = post_floyd['date'], post_floyd['cases']
+    # Linear Regression
+    pre_floyd = minnesota_numeric.loc[:'2020-05-25']
+    post_floyd = minnesota_numeric.loc['2020-05-26':]
+    pre_floyd_x, pre_floyd_y = pre_floyd[['date']], pre_floyd['cases']
+    post_floyd_x, post_floyd_y = post_floyd[['date']], post_floyd['cases']
 
-    model = LinearRegression()
+    model = LinearRegression(fit_intercept=False)
     model.fit(pre_floyd_x, pre_floyd_y)
-    pred = model.predict(post_floyd_x)
-    print(pred)
+    pred = model.predict(minnesota_numeric[['date']])
 
-    # print(pre_floyd)
-    # print('=' * 50)
-    # print(post_floyd)
+    pred_df = pd.DataFrame({'predictions': pred})
+    pred_df.index = minnesota.index
+    print(pred_df)
+
+    fig, ax = plt.subplots(1)
+    minnesota['cases'].plot(ax=ax, ylim=0)
+    pred_df.plot(ax=ax, ylim=0)
+    ax.set_title('Minnesota Cases')
+
+    fig.savefig('minnesota_cases.png')
 
 
 if __name__ == '__main__':
     main()
+
